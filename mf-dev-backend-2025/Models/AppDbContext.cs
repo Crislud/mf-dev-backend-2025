28737208a156dee72bfc69af5fe60b99c9b21a85
Cristina <ludimilasantos175@gmail.com>
2025-04-28 19:02:48 -0300

    Configuração do banco de dados e da tabela veículos

---
+++ mf-dev-backend-2025/Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using mf_dev_backend_2025.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace mf_dev_backend_2025
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Veiculo> Veiculos { get; set; }
+    }
+}
